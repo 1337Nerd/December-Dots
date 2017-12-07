649cd04655665c5f6bb63734383dc54da03d7c39
1337Nerd <26494273+1337Nerd@users.noreply.github.com>
2017-12-07 17:19:28 -0500

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,26 @@
-wal -t -r &
-
 neofetch
 
 export EDITOR='vim'
 
 set fish_greeting
+set fish_color_command cb4b16
 
 function fish_prompt
     echo (set_color 403434) '------------------------------------------------------------'
     echo (set_color 087DC0)(pwd|sed "s=$HOME=~=")(set_color 847BE4)' » '(set_color normal)
 end
+
+alias ...='cd ../..'
+alias update='pacaur -Syu --noconfirm; pacaur -Rns (pacaur -Qttdq) --noconfirm; pacaur -Sc --noconfirm'
+
+function configure_pager
+	set -gx LESS_TERMCAP_mb \e"[01;31m" 
+	set -gx LESS_TERMCAP_md \e"[1;31m" 
+	set -gx LESS_TERMCAP_me \e"[0m"
+	set -gx LESS_TERMCAP_se \e"[0m" 
+	set -gx LESS_TERMCAP_so \e"[1;44;33m"
+	set -gx LESS_TERMCAP_ue \e"[0m"
+	set -gx LESS_TERMCAP_us \e"[1;32m"
+end
+
+configure_pager
